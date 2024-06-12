60a337b29cd5a25528e2e481eac021b8aea2cf07
oleglazeba <oleg.lazeba@antoshka.ua>
2024-06-12 08:52:52 +0300

    update

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -16,7 +16,7 @@ def set_driver(pytestconfig, request):
     if browser == "chrome":
         chromedriver_autoinstaller.install()
         options = webdriver.ChromeOptions()
-        # options.add_argument("--headless")
+        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--window-size=1920,1080")
