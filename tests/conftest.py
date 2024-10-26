bd9255132b107674e7212a824638798abef7fc10
oleglazeba <oleg.lazeba@antoshka.ua>
2024-10-26 20:33:43 +0300

    some update

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ add the "environment" variable to "pytestconfig" with the "pytest_addoption" hoo
 
 
 def pytest_addoption(parser):
-    parser.addoption("--environment", action="store", default="dev", help="choose environment", choices=("qa", "dev"))
+    parser.addoption("--environment", action="store", default="qa", help="choose environment", choices=("qa", "dev"))
     parser.addoption("--browser_name", action="store", default="chrome", help="choose browser",
                      choices=("chrome", "firefox"))
 
--- tests/ui/test_registration_new_user.py
+++ tests/ui/test_registration_new_user.py
@@ -8,10 +8,11 @@ from tests.ui.conftest import BaseTest
 
 class TestRegistrationUser(BaseTest):
     @allure.step("Try to registration registered user AWDEV-T506")
-    @pytest.mark.parametrize("user", [RegistrationUserUi.registered_user], ids=str)
-    def test_try_registration_new_user_awdev_t506(self, user, set_env_settings):
-        env = set_env_settings.env
-        print(env)
+    def test_try_registration_new_user_awdev_t506(self, set_env_settings):
+        print(set_env_settings.env)
+        print(set_env_settings.base_url)
+
+        user = RegistrationUserUi.registered_user
         self.registration_page.open_registration_form()
         self.registration_page.fill_registration_form(user=user)
         self.registration_page.verify_phone_warning()
